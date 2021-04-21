dfde139981ae5cc7f4bc85bb42d03f86e14f88e3
aadarshkrishnan <akrish49@illinois.edu>
2021-04-20 21:46:46 -0500

    Implemented interface queen movement

--- src/queen.cc
+++ src/queen.cc
@@ -1,12 +1,48 @@
 #include "queen.h"
+#include "space.h"
+
 
 namespace chess {
 
     bool Queen::Move(int new_x_position, int new_y_position, Board board) {
-        return false;
+        //if it's not a straight move
+        if (current_position_x_ != new_x_position || current_position_y_ != new_y_position) {
+            //it has to be diagonal
+            if (abs(current_position_x_ - new_x_position) != abs(current_position_y_ - new_y_position)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     bool Queen::CheckPossibleMove(int new_x_position, int new_y_position, Board board) {
-        return false;
+        // horizontal movement
+        if (current_position_x_ == new_x_position) {
+            int yIncrement = (new_y_position - current_position_y_) / (abs(new_y_position - current_position_y_));
+            for (int i = current_position_y_ + yIncrement; i != new_y_position; i += yIncrement) {
+                if (typeid(board.getBoard()[new_x_position][i].getColor()) != typeid(Space())) {
+                    return false;
+                }
+            }
+            //vertical movement
+        } else if (current_position_y_ == new_y_position) {
+
+            int xIncrement = (new_x_position - current_position_x_) / (abs(new_x_position - current_position_x_));
+            for (int i = current_position_x_ + xIncrement; i != new_x_position; i += xIncrement) {
+                if (typeid(board.getBoard()[i][new_y_position].getColor()) != typeid(Space())) {
+                    return false;
+                }
+            }
+        }
+        //diagonal movement
+        int xIncrement = (current_position_x_ - new_x_position) / (abs(current_position_x_ - new_x_position));
+        int yIncrement = (current_position_y_ - new_y_position) / (abs(current_position_y_ - new_y_position));
+
+        for (int i = 1; i < abs(new_x_position - current_position_x_); i++) {
+            if (board.getBoard()[new_x_position + xIncrement*i][new_y_position + yIncrement*i].getColor() != 2) {
+                return false;
+            }
+        }
+        return true;
     }
 }
