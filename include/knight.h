cf62bdbfe5f6ee80c0dc91fd9e0c4d90ab8191c2
aadarshkrishnan <akrish49@illinois.edu>
2021-04-20 20:18:53 -0500

    Implemented interface knight movement

--- include/knight.h
+++ include/knight.h
@@ -6,6 +6,7 @@ namespace chess {
 
     class Knight: public Piece {
     public:
-        bool Move(int new_x_position, int new_y_position);
+        bool Move(int new_x_position, int new_y_position, Board board) override;
+        bool CheckPossibleMove(int new_x_position, int new_y_position, Board board) override;
     };
 }  // namespace chess
--- src/knight.cc
+++ src/knight.cc
@@ -2,7 +2,18 @@
 
 namespace chess {
 
-    bool Knight::Move(int new_x_position, int new_y_position) {
+    bool Knight::Move(int new_x_position, int new_y_position, Board board) {
+        if ((abs(current_position_x_ - new_x_position) == 2 && abs(current_position_y_ - new_y_position) == 1) ||
+        (abs(current_position_x_ - new_x_position) == 1 && abs(current_position_y_ - new_y_position) == 2))
+        {
+            return true;
+        }
         return false;
+    }
+
+    bool Knight::CheckPossibleMove(int new_x_position, int new_y_position, Board board) {
+        if (board.getBoard()[new_x_position][new_y_position].getColor() != board.getBoard()[current_position_x_][current_position_y_]) {
+            return true;
+        }
     }
 }
