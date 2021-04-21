fe2201ddf0f9d46002306b6058a6f642e5646212
aadarshkrishnan <akrish49@illinois.edu>
2021-04-20 19:59:19 -0500

    Implemented interface king movement

--- include/king.h
+++ include/king.h
@@ -6,8 +6,9 @@ namespace chess {
 
     class King : public Piece {
     public:
-        bool Move(int new_x_position, int new_y_position);
+        bool Move(int new_x_position, int new_y_position, Board board) override;
+        bool CheckPossibleMove(int new_x_position, int new_y_position, Board board) override;
 
-        bool CheckCastle();
+        bool CheckCastle(int new_x_position, int new_y_position, Board board);
     };
 }  // namespace chess
--- src/king.cc
+++ src/king.cc
@@ -2,7 +2,27 @@
 
 namespace chess {
 
-    bool King::Move(int new_x_position, int new_y_position) {
+    bool King::CheckPossibleMove(int new_x_position, int new_y_position, Board board) {
+        if (board.getBoard()[new_x_position][new_y_position].getColor() != 2) {
+            return false;
+        }
+        return true;
+    }
+
+    bool King::Move(int new_x_position, int new_y_position, Board board) {
+        if (abs(current_position_x_ - new_x_position) <= 1 && abs(current_position_y_ - new_y_position) <= 1) {
+            return true;
+        }
         return false;
+    }
+
+    bool King::CheckCastle(int new_x_position, int new_y_position, Board board) {
+        if (current_position_x_ == 0 || current_position_x_ == 7) {
+            if (current_position_y_ == 4) {
+                //Queenside castle have to check queenside back rank
+
+                //Kingside castle have to check kingside back rank
+            }
+        }
     }
 }
